98a7f8aa4abf6340951595d4510866a3cdc6169d
nikita lakhov <kesarbin1@yandex.ru>
2022-07-23 17:00:32 +0300

    pagination homework

--- src/components/bookmark.jsx
+++ src/components/bookmark.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const BookMark = ({ status, ...rest }) => {
-  return (
-    <button {...rest}>
-      <i className={"bi bi-arrow-through-heart" + (status ? "-fill" : "")}></i>
-    </button>
-  );
+    return (
+        <button {...rest}>
+            <i
+                className={
+                    "bi bi-arrow-through-heart" + (status ? "-fill" : "")
+                }
+            ></i>
+        </button>
+    );
+};
+
+BookMark.propTypes = {
+    status: PropTypes.bool.isRequired
 };
 
 export default BookMark;
